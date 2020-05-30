9f0b61bb42646c69c7e4fa88932f39d2f6e38174
Dev <owlman1911@gmail.com>
2020-05-30 07:45:52 -0400

    removed the schedule for asg scaling

--- services/webserver-cluster/aws-asg.tf
+++ services/webserver-cluster/aws-asg.tf
@@ -40,19 +40,5 @@ resource "aws_autoscaling_group" "asg-web" {
     }
 }
 
-resource "aws_autoscaling_schedule" "scale_outof_bus" {
-    scheduled_action_name           = "scale-out-bus-hrs"
-    min_size                        = 2
-    max_size        = 4
-    desired_capacity   = 4
-    recurrence          = "0 9 * * *" 
-}
 
-resource "aws_autoscaling_schedule" "scale_in_night" {
-    scheduled_action_name           = "scale-in-at-ngt"
-    min_size                        = 1
-    max_size        = 4
-    desired_capacity   = 1
-    recurrence          = "0 17 * * *" 
-}
 
